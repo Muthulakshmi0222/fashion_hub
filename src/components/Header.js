bbdfb7db6889ebdd8cb9339807ab5e4ae013714b
muthulakshmim <MY_NAME@example.com>
2021-11-14 22:44:17 +0530

    cart

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="block row center">
+      <div>
+          <h1>Shopping Cart</h1>
+      </div>
+      </header>
+  );
+}
